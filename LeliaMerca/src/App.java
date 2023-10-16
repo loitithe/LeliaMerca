a799e192903f69ab5af81a5ed123074017567932
rguido <rodrigoguidoarias92@gmail.com>
2023-10-16 19:43:35 +0200

    primer commit

---
+++ LeliaMerca/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Principal p = new Principal();
+        p.setVisible(true);
+        p.setSize(600, 400);
+        p.setLocationRelativeTo(null);
+    }
+}
