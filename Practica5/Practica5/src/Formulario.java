af2b58a19c2ad93c7bf63372397bd3d3c4109b1c
rguido <rodrigoguidoarias92@gmail.com>
2023-10-23 19:49:19 +0200

    metodo colocacontraints no funciona

--- Practica5/Practica5/src/Formulario.java
+++ Practica5/Practica5/src/Formulario.java
@@ -13,6 +13,7 @@ public class Formulario extends JFrame {
             txtPrimeraPosicionVocal;
     Font fuenteTitulo, fuenteSecundaria;
     JButton btnProcesarFrase, btnLimpiar, btnSalir;
+    Container container;
     GridBagConstraints c;
     GridBagLayout gbl;
 
@@ -25,6 +26,38 @@ public class Formulario extends JFrame {
     }
 
     private void initComponents() {
+        c = new GridBagConstraints();
+        gbl = new GridBagLayout();
+        container = this.getContentPane();
+        container.setLayout(gbl);
+
+        fuenteTitulo = new Font("Arial", Font.BOLD, 20);
+        fuenteSecundaria = new Font("arial", Font.BOLD, 14);
+
+        // primera fila
+        lblIntroduceFrase = new JLabel("Introduce una frase");
+        lblIntroduceFrase.setFont(fuenteTitulo);
+        // c.gridx = 0;
+        // c.gridy = 0;
+        container.add(lblIntroduceFrase, colocaConstraints(0, 0, 0, 0, 0, 0, 0));
+        txtIntroduceFrase = new JTextField("");
+
+        // c.gridx = 1;
+        // c.gridy = 0;
+        container.add(txtIntroduceFrase, colocaConstraints(1, 0, 0, 0, 0, 0, 0));
+
+    }
+
+    public GridBagConstraints colocaConstraints(int gx, int gy, int gw, int gh, double wx, double wy, int fill) {
+        GridBagConstraints c = new GridBagConstraints();
+        c.gridx = gx; // coordenadas
+        c.gridy = gy; // coordenadas
+        c.gridwidth = gw; // celdas de ancho
+        c.gridheight = gh; // celdas de alto
+        c.weightx = wx; // gestion de espacio disponible 0-1
+        c.weighty = wy; // gestion de espacio disponible 0-1
+        c.fill = fill; // rellenar
+        return c;
 
     }
 
